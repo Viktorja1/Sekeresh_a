3beee1714e327a6a6f558788f242618c6051e08c
Viktorja1 <30689417+Viktorja1@users.noreply.github.com>
2018-07-16 16:09:44 +0300

    ++

--- app/code/local/Sekeresh/Action/sql/sekeresh_action/upgrade-0.1.1-0.1.2.php
+++ app/code/local/Sekeresh/Action/sql/sekeresh_action/upgrade-0.1.1-0.1.2.php
@@ -24,8 +24,8 @@ $table = $installer->getConnection()->newTable($tableNameActionProduct)
         'primary' => true,
         'unsigned' => true,
     ), 'Product ID')
-    ->addForeignKey('FK_ID_ACTION','action_id',$actionTable, 'action_id', Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE)
-    ->addForeignKey('FK_ID_PRODUCT','product_id',$ProductTable, 'entity_id', Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE);
+    ->addForeignKey('FK_ACTION_PRODUCT_ACTION','action_id',$actionTable, 'action_id', Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE)
+    ->addForeignKey('FK_ACTION_PRODUCT_PRODUCT','product_id',$ProductTable, 'entity_id', Varien_Db_Ddl_Table::ACTION_CASCADE, Varien_Db_Ddl_Table::ACTION_CASCADE);
 
 $installer->getConnection()->createTable($table);
 
